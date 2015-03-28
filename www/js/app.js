727b02d59a72cfe63a3f486b38eb3d28c7280529
root <root@localhost.localdomain>
2015-03-28 12:08:54 -0400

    added localstorage for devices
        also checks for existance of sessionid

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,12 @@ var app = (function ()
 
     app.launch = function () {
         //preload images
+    
+        app.sessionid = window.localStorage.getItem("sessionid");
+        if(app.sessionid == null){
+            app.sessionid = "0";
+        }
+
         communicate({token: token, mode: 'get_values_categories'}, function (data) {
             imgs = [];
             $.each(data.categories, function () {
