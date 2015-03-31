4d174a26192ad8d894986cb7e1aa4f96fb9b6134
Merijn van Wouden <ik@merijn.pro>
2015-04-01 01:41:09 +0200

    pre merge

--- www/js/app.js
+++ www/js/app.js
@@ -73,7 +73,7 @@ var app = (function ()
                     '<span class="duration" data-duration="' + this.duration_sec +
                     '" data-dur-sec="' + this.duration_notation + '">' +
                     this.duration_notation + '</span><u><i></i><u></u><b></b></u></div>'
-                    ).click(songClick).swipe({swipeStatus:songSwipeStatus,allowPageScroll:"vertical"}).appendTo(li);
+                    ).click(songClick).swipe({swipeStatus:songSwipeStatus,allowPageScroll:"vertical"}).bind('touchstart',songTouchStart).appendTo(li);
             if (this.in_favourite == "true") {
                 li.addClass('faved');
             }
